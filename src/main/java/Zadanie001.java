badefce14788824ded2679412b5f43fcff42fd80
Karoru4 <Karl2188@>
2023-11-07 16:24:51 +0100

    Poprawiony commit

--- src/main/java/Zadanie001.java
+++ src/main/java/Zadanie001.java
@@ -2,5 +2,7 @@ public class Zadanie001 {
     public static void main(String[]args){
         System.out.println("Uczę\n" +
                 "się\n" +
-                "programowania"); }
+                "programowania\n" +
+                "i\n" +
+                "commitowania"); }
 }
